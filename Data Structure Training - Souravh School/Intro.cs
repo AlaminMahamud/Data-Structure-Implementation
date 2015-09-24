983a124a4ca7e62a987f222be479140db56a5f6f
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2015-09-24 13:58:51 +0600

    Add(Sourabh School) :: Introduction To Data Structures

---
+++ Data Structure Training - Souravh School/Intro.cs
@@ -0,0 +1,38 @@
+/*
+	
+	Introduction To Data Structure
+	------------------------------
+
+	# Implementation
+		# dictionary - words are sorted
+		# Map Data
+		# Cash Memo
+
+	# Data Structures
+		data structure is a way to store and organize data in a computer so that it can 
+		be used efficiently
+	
+	# Data Structure Analysis
+		# Mathematical Or Logical Models
+		|
+		# Abstract Data Types
+
+	# List - ADT
+		# Store a Given Number of Elements
+		# Read Elements By Position
+		# Modify Element Position
+	# Arrays - Concrete Implementation
+
+	Implementation Means Concrete
+
+	# ADT
+		Define Data And Operations
+		No Implementations
+
+	# Studying Data Structures
+		Logical View
+		Operations
+		Cost Of Operations
+		Implementation
+
+*/
