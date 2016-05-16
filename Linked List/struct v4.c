3123d0e3fc60f923cfa79f33d9c760d22b6e3a7f
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-05-16 19:57:16 +0600

    struct v4.c

---
+++ Linked List/struct v4.c
@@ -0,0 +1,52 @@
+
+struct node
+{
+	int roll;
+	node * next;
+};
+
+node *root = null;
+
+
+
+void print()
+{
+	node *current = root;
+	while(current->next != null)
+	{
+		printf("%d\n",current->roll);
+		current = current->next;
+	}
+}
+
+void append(int roll)
+{
+	if(root == null)
+	{
+		root = new node();
+		root->next = null;
+	}
+	else
+	{
+		node * current = root;
+		while(current->next!=null)
+		{
+			current = current->next; 
+		}
+
+		node *newnode = new node();
+		newnode->roll = roll;
+		newnode->next = null;
+
+		current_node->next = newnode;
+	}
+}
+
+int main()
+{
+	append(1);
+	append(2);
+	append(3);
+	append(4);
+	print();
+}
