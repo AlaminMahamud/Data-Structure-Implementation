7778b139505fed9198545d2f55526a28a9feb117
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-05-19 09:33:00 +0600

    a basic example of struct

---
+++ Struct/struct 4.c
@@ -0,0 +1,11 @@
+// a basic struct example
+
+struct example 
+{
+	int x;
+}
+
+struct example an_example;  // Treating it like a normal variable type 
+							// except with the addition of struct
+
+an_example.x = 33;			// How to access its members
